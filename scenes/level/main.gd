4c09eb7b6fe5f52ff670a197f8235e8f87377fd6
EeVii <Nomenaharizo468@gmail.com>
2026-09-17 23:58:30 +0300

    story/ prologue_placeholder
    added placeholder art for the prologue
    added placeholder art for the main menu
    added credit menu

--- scenes/level/main.gd
+++ scenes/level/main.gd
@@ -4,4 +4,4 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	await get_tree().create_timer(1).timeout
-	Dialogic.start("intro_prologue", 0)
+	Dialogic.start("Prologue", 0)
--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -2,11 +2,11 @@ extends CanvasLayer
 
 @export var option_menu_ref: PackedScene
 @export var quit_box_ref: PackedScene
+@export var credit_menu_ref: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
 
 
 func _on_start_button_pressed() -> void:
@@ -21,3 +21,8 @@ func _on_option_button_pressed() -> void:
 func _on_quit_button_pressed() -> void:
 	var new_quit_box = quit_box_ref.instantiate()
 	add_child(new_quit_box)
+
+
+func _on_credits_button_pressed() -> void:
+	var new_credit_menu = credit_menu_ref.instantiate()
+	add_child(new_credit_menu)
