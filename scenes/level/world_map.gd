3ea7d6b6c21a79503fb490af6ff18d2a20b9d82f
EeVii <Nomenaharizo468@gmail.com>
2026-09-23 00:28:39 +0300

    story: update
    added all the timelines
    added all the characters]
    added the world map feature and buttons
    implemented dialog for day 1.

--- scenes/level/world_map.gd
+++ scenes/level/world_map.gd
@@ -4,12 +4,39 @@ extends CanvasLayer
 ## day_01_main_road
 ## keep the naming consistent
 
+@onready var location_buttons: Dictionary = {
+	"town_center": $Buttons/TownCenter,
+	"lake_side" : $Buttons/LakeSide,
+	"main_road" : $Buttons/MainRoad,
+	"cemetery" : $Buttons/Cemetery,
+	"forest" : $Buttons/Forest,
+	"hotel" : $Buttons/Hotel
+}
+
+var location_available_day: Dictionary = {
+	1: ["town_center","main_road", "lake_side"],
+	2: ["hotel", "cemetery", "lake_side"],
+	3: ["main_road", "forest", "hotel"],
+	4: ["cemetery", "main_road", "forest"],
+	5: ["hotel", "town_center", "lake_side"],
+	6: ["cemetery", "town_center", "forest"]
+}
+
 var current_day: int = 0
 
+func _ready() -> void:
+	_update_available_locations()
+	
+func _update_available_locations() -> void:
+	current_day = Dialogic.VAR.get_variable("ChapterTracking.current_day")
+	var open_locations: Array = location_available_day.get(current_day, [])
+	for location_id in location_buttons.keys():
+		var button: Button = location_buttons[location_id]
+		button.visible = location_id in open_locations
 
 func _go_to_location(location_id: String) -> void:
 	current_day = Dialogic.VAR.get_variable("ChapterTracking.current_day")
-	var timelime_name: String = "day_0%01d_%s" % [current_day, location_id]
+	var timelime_name: String = "day_0%1d_%s" % [current_day, location_id]
 	print(timelime_name)
 	get_tree().current_scene.queue_free()
 	Dialogic.start(timelime_name)
@@ -23,16 +50,15 @@ func _on_lake_side_pressed() -> void:
 
 
 func _on_main_road_pressed() -> void:
-	pass # Replace with function body.
+	_go_to_location("main_road")
 
 
 func _on_cemetery_pressed() -> void:
-	pass # Replace with function body.
+	_go_to_location("cemetery")
 
 
 func _on_forest_pressed() -> void:
-	pass # Replace with function body.
-
+	_go_to_location("forest")
 
 func _on_hotel_pressed() -> void:
-	pass # Replace with function body.
+	_go_to_location("hotel")
